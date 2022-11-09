2ff5b5cac4e5c80ea8adc5e30f4584273e3c5587
quooctrangg <nguyenquoctrang188@gmail.com>
2022-11-09 23:34:19 +0700

    update product

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 let lists = document.getElementsByClassName('slider-item')
+// console.log(lists);
 
 const next = () => {
     document.getElementById('slider').appendChild(lists[0])
-    lists[3].style.animation = 'btn-control-animation-show-next  ease-in-out 0.5s forwards'
+    lists[3].style.animation = 'btn-control-animation-show-next  ease-in-out 1s forwards'
     hidden()
 }
 
 const prev = () => {
     document.getElementById('slider').prepend(lists[lists.length - 1])
-    lists[0].style.animation = 'btn-control-animation-show-prev ease-in-out 0.5s forwards'
+    lists[0].style.animation = 'btn-control-animation-show-prev ease-in-out 1s forwards'
     hidden()
 }
 
